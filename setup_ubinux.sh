0934dd49fa5f89267309d62b44da338e75602f71
Lei Maohui <leimaohui@cn.fujitsu.com>
2021-02-24 15:48:25 +0900

    setup_ubinux.sh: Update for ubinux2021.03.
    Signed-off-by: Lei Maohui <leimaohui@cn.fujitsu.com>

--- setup_ubinux.sh
+++ setup_ubinux.sh
@@ -13,7 +13,7 @@ fi
 
 MACHINE=$1
 BUILD=$2
-DOWNLOADS=/ubinux-dev/common/downloads/ubinux202102
+DOWNLOADS=/ubinux-dev/common/downloads/202103
 export TEMPLATECONF="meta-ubinux/conf"
 
 #. trunk/poky-ubinux/oe-init-build-env ${BUILD}
