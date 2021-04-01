8b9c60e6f01ea7b165c8783e197fea103ffb8a93
Wang Mingyu <wangmy@cn.fujitsu.com>
2021-04-01 16:59:14 +0900

    Updated scripts for ubinux2021.04.
    Signed-off-by: Wang Mingyu <wangmy@cn.fujitsu.com>

--- setup_ubinux.sh
+++ setup_ubinux.sh
@@ -13,12 +13,12 @@ fi
 
 MACHINE=$1
 BUILD=$2
-DOWNLOADS=/ubinux-dev/common/downloads/202104
 export TEMPLATECONF="meta-ubinux/conf"
 
 #. trunk/poky-ubinux/oe-init-build-env ${BUILD}
 . ./oe-init-build-env ${BUILD}
 
+DOWNLOADS=$(readlink -f $2)/downloads/downloads-ubinux202104
 add_conf_append ()
 {
 cat << EOF >> conf/local.conf
