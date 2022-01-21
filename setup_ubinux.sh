ee6d087eef835235e26cfe03be78bd168476ec23
Lei Maohui <leimaohui@fujitsu.com>
2022-01-21 10:07:05 +0900

    setup_ubinux.sh: Updated for ubinux2022.02.
    Signed-off-by: Lei Maohui <leimaohui@fujitsu.com>

--- setup_ubinux.sh
+++ setup_ubinux.sh
@@ -13,7 +13,7 @@ fi
 
 MACHINE=$1
 BUILD=$2
-DOWNLOADS=/ubinux-dev/ubinux003/common/downloads/202201/
+DOWNLOADS=/ubinux-dev/ubinux003/common/downloads/202202/
 export TEMPLATECONF="meta-ubinux/conf"
 
 #. trunk/poky-ubinux/oe-init-build-env ${BUILD}
