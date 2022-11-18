36e597f02b7179408388d4dbd2656998bb08dd8c
Wang Mingyu <wangmy@cn.fujitsu.com>
2022-11-18 15:38:18 +0900

    ubinux 2022.11 released.
    Signed-off-by: Wang Mingyu <wangmy@cn.fujitsu.com>

--- setup_ubinux.sh
+++ setup_ubinux.sh
@@ -13,12 +13,12 @@ fi
 
 MACHINE=$1
 BUILD=$2
-DOWNLOADS=/ubinux-dev/ubinux003/common/downloads/202211/
 export TEMPLATECONF="meta-ubinux/conf/templates/default/"
 
 #. trunk/poky-ubinux/oe-init-build-env ${BUILD}
 . ./oe-init-build-env ${BUILD}
 
+DOWNLOADS=$(readlink -f $2)/downloads/downloads-ubinux202211
 add_conf_append ()
 {
 cat << EOF >> conf/local.conf
